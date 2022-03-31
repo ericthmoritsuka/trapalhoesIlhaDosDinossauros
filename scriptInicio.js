ea89ab8df3a9cde4f64dfb23ec2913790a47f817
ericthmoritsuka <ericthmoritsuka@gmail.com>
2022-03-31 19:51:36 -0300

    ajustes na pagina

--- scriptInicio.js
+++ scriptInicio.js
@@ -1,10 +1,12 @@
+//inicio
 const botao = document.querySelector("button");
 const inputNome = document.querySelector(".inputNome");
 const jsTexto = document.querySelector(".jsTexto");
+let nome = "";
 
 const salvaNome = () => {
+  nome = document.querySelector("input").value;
   if (document.querySelector("input").value.length > 0) {
-    const nome = document.querySelector("input").value;
     inputNome.classList.toggle("ativo");
     document.querySelector(".nomeDoJogador").innerHTML = nome;
     jsTexto.classList.toggle("ativo");
@@ -12,3 +14,9 @@ const salvaNome = () => {
 };
 
 botao.addEventListener("click", salvaNome);
+
+export { nome };
+
+//checando se há um nome para continuar
+const img = document.querySelector('[src^="./img/Didiana"]');
+const a = document.querySelector("a");
